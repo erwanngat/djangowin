5a438be5318f114190c0979f02b4f01241bc9d3c
erwanngat <ewnran69@yahoo.fr>
2024-04-05 15:02:42 +0200

    front

--- djangoWin/settings.py
+++ djangoWin/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = '/home/ewnran/dev/djangowin/collected_statics/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
